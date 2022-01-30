8b2bebb9c7219f93bc56e28c056ad1bad00bfa55
larissavilelasobral <larissavilelasobral@gmail.com>
2022-01-30 19:03:40 -0300

    refinamento

--- main.py
+++ main.py
@@ -11,22 +11,14 @@ import pandas as pd
 
 tabela_vendas = pd.read_excel('Vendas.xlsx')
 
-# faturamento por loja
 pd.set_option('display.max_columns', None)
 faturamento = tabela_vendas[['ID Loja', 'Valor Final']].groupby('ID Loja').sum()
-print(faturamento)
-print('-' * 50)
 
-# quantidade de produtos vendidos por loja
 quantidade = tabela_vendas[['ID Loja', 'Quantidade']].groupby('ID Loja').sum()
-print(quantidade)
-print('-' * 50)
 
-# ticket medio por produto em cada loja
 ticket_medio = (faturamento['Valor Final'] / quantidade['Quantidade']).to_frame()
-print(ticket_medio)
+ticket_medio = ticket_medio.rename(columns={0: 'Ticket Médio'})
 
-# enviar um email com o relatório
 EMAIL_ADDRESS = email_de_origem
 EMAIL_PASSWORD = senha
 
@@ -37,14 +29,14 @@ msg['To'] = email_de_destino
 msg.attach(MIMEText(f''' 
 <h2>Bom dia!</h2>
 
-<h2>Faturamento:</h2>
-{faturamento.to_html()}
+<h3>Faturamento:</h3>
+{faturamento.to_html(formatters={'Valor Final': 'R${:,.2f}'.format})}
 
-<h2>Quantidade Vendida:</h2>
+<h3>Quantidade Vendida:</h3>
 {quantidade.to_html()}
 
-<h2>Ticket Medio dos Produtos em cada Loja:</h2>
-{ticket_medio.to_html()}
+<h3>Ticket Medio dos Produtos em cada Loja:</h3>
+{ticket_medio.to_html(formatters={'Ticket Médio': 'R${:,.2f}'.format})}
 
 Obrigado!
 ''', 'html'))
